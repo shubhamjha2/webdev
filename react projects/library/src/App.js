69726a29ae997dc9051c064dff19dcf4f292a0bc
shubham jha <116482692+shubhamjha2@users.noreply.github.com>
2024-02-28 19:20:16 +0530

    currencychangeradded

--- react projects/library/src/App.js
+++ react projects/library/src/App.js
@@ -12,7 +12,7 @@ function App() {
       {/* <Bgchanger /> */}
       {/* <Dropdowncards /> */}
       {/* <Passwordgen /> */}
-      {Currenccy_changer}
+      <Currenccy_changer />
     </>
   );
 }
---
+++ react projects/library/src/hooks/Usecurrencyinfo.js
@@ -0,0 +1,19 @@
+import {useEffect,useState} from 'react'
+
+function Usecurrencyinfo(currency) {
+    const [data,setdata]=useState({});
+    useEffect(() => {
+      fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+      .then((pro)=>pro.json())
+      .then((pro)=>setdata(pro[currency])) 
+      .catch(error => {
+        console.error('Failed to fetch with error:', error);
+      });
+      
+    
+    }, [currency])
+    return data;
+  
+}
+
+export default Usecurrencyinfo;
